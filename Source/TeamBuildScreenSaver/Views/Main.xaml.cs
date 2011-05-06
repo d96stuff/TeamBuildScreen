cecac16a020b8d805dccabb8643042d02affc655
Jim Liddell <liddellj@users.noreply.github.com>
2011-05-06 08:03:40 +0000

    Added manually created release branches back in. Branch command in TFS wouldn't work because the 'Main' folder didn't originally exist.

---
+++ Source/TeamBuildScreenSaver/Views/Main.xaml.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Main.xaml.cs" company="Jim Liddell">
+//    Copyright © Jim Liddell. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TeamBuildScreenSaver.Views
+{
+    public partial class Main
+    {
+        #region Constructors
+
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
