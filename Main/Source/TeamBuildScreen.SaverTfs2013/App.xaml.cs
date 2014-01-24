0c53192af8e12648b109804ad9cd7b15ace99916
StaffanW_cp <SND\StaffanW_cp>
2014-01-24 13:17:32 +0000

    * Added support for TFS/VS 2013 Thanks to jeffras1234 for input

---
+++ Main/Source/TeamBuildScreen.SaverTfs2013/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Jim Liddell">
+//    Copyright © Jim Liddell. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TeamBuildScreen.SaverTfs2013
+{
+    #region Usings
+
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+    using TeamBuildScreen.Core.Models;
+    using TeamBuildScreen.Tfs2013.Models;
+    using TeamBuildScreen.Core;
+
+    #endregion
+
+	public partial class App : System.Windows.Application
+    {
+        #region Fields
+
+        private TeamBuildScreenSaver screenSaver;
+
+        #endregion
+
+        #region Methods
+
+        private void OnStartup(Object sender, StartupEventArgs e)
+        {
+            IBuildServerService service = new BuildServerService();
+            IDomainProjectPicker projectPicker = new TeamProjectPickerAdapter();
+            this.screenSaver = new TeamBuildScreenSaver(service, projectPicker);
+
+            string[] args = e.Args;
+
+            this.screenSaver.Startup(args);
+        }
+        
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            this.screenSaver.Exit();
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // TODO: Log details
+            MessageBox.Show(
+                string.Format(
+                "An error has been detected in the application that has caused the application to shutdown:\n\n{0}\n\nApologies for any inconvenience.",
+                e.Exception.Message),
+                "Team Build Screen Saver",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            e.Handled = true;
+
+            if (Application.Current != null)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Main/Source/TeamBuildScreen.Tfs2013/Models/BuildStatusConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace TeamBuildScreen.Tfs2013.Models
+{
+    public static class BuildStatusConverter
+    {
+        public static Core.Models.BuildStatus Convert(BuildStatus status)
+        {
+            switch (status)
+            {
+                case BuildStatus.Failed:
+                    return Core.Models.BuildStatus.Failed;
+                case BuildStatus.InProgress:
+                    return Core.Models.BuildStatus.InProgress;
+                case BuildStatus.NotStarted:
+                    return Core.Models.BuildStatus.NotStarted;
+                case BuildStatus.PartiallySucceeded:
+                    return Core.Models.BuildStatus.PartiallySucceeded;
+                case BuildStatus.Stopped:
+                    return Core.Models.BuildStatus.Stopped;
+                case BuildStatus.Succeeded:
+                    return Core.Models.BuildStatus.Succeeded;
+                default:
+                    throw new NotSupportedException("Unable to convert unrecognised BuildStatus.");
+            }
+        }
+    }
+}
