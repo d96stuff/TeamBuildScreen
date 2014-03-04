8fc12636b294a44379b893beedb2415184ce73f4
StaffanW_cp <SND\StaffanW_cp>
2014-03-04 07:52:43 +0000

    * Updated version of installation packages to 2.1.0 in Main branch

--- Main/Source/TeamBuildScreen.Tfs2010/Models/BuildStatusConverter.cs
+++ Main/Source/TeamBuildScreen.Tfs2010/Models/BuildStatusConverter.cs
@@ -14,22 +14,16 @@ namespace TeamBuildScreen.Tfs2010.Models
             {
                 case BuildStatus.Failed:
                     return TeamBuildScreen.Core.Models.BuildStatus.Failed;
-                    break;
                 case BuildStatus.InProgress:
                     return TeamBuildScreen.Core.Models.BuildStatus.InProgress;
-                    break;
                 case BuildStatus.NotStarted:
                     return TeamBuildScreen.Core.Models.BuildStatus.NotStarted;
-                    break;
                 case BuildStatus.PartiallySucceeded:
                     return TeamBuildScreen.Core.Models.BuildStatus.PartiallySucceeded;
-                    break;
                 case BuildStatus.Stopped:
                     return TeamBuildScreen.Core.Models.BuildStatus.Stopped;
-                    break;
                 case BuildStatus.Succeeded:
                     return TeamBuildScreen.Core.Models.BuildStatus.Succeeded;
-                    break;
                 default:
                     throw new NotSupportedException("Unable to convert unrecognised BuildStatus.");
             }
