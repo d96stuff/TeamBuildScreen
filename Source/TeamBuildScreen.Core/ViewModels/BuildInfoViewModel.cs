740f743bfc49ab6489d1292176b12b3aa05674e7
Staffan Westin <d96stuff@hotmail.com>
2015-06-24 14:34:35 +0000

    * New feature: The number of failed tests shown as big numbers on the build status icon.

--- Source/TeamBuildScreen.Core/ViewModels/BuildInfoViewModel.cs
+++ Source/TeamBuildScreen.Core/ViewModels/BuildInfoViewModel.cs
@@ -6,382 +6,294 @@
 
 namespace TeamBuildScreen.Core.ViewModels
 {
-    #region Usings
+	#region Usings
 
-    using System;
-    using System.ComponentModel;
-    using TeamBuildScreen.Core.Models;
+	using System;
+	using System.ComponentModel;
+	using TeamBuildScreen.Core.Models;
 
-    #endregion
+	#endregion
 
-    public class BuildInfoViewModel : INotifyPropertyChanged
-    {
-        #region Fields
-
-        /// <summary>
-        /// The model underlying the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        private BuildInfoModel dataModel;
-
-        /// <summary>
-        /// The description for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        private string description;
-
-        /// <summary>
-        /// The name of the person who requested the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        private string requestedBy;
-
-        /// <summary>
-        /// The time when the build was started.
-        /// </summary>
-        private DateTime? startedOn;
+	public class BuildInfoViewModel : INotifyPropertyChanged
+	{
+		#region Properties
 
 		/// <summary>
-		/// Formatted string for status.
+		/// The model underlying the <see cref="BuildInfoViewModel"/>.
 		/// </summary>
-	    private string latestStatus;
-
-        /// <summary>
-        /// The time the build was completed.
-        /// </summary>
-        private DateTime? completedOn;
-
-        /// <summary>
-        /// The test results for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        private string testResults;
+		public BuildInfoModel DataModel { get; set; }
 
 		/// <summary>
-		/// The bottom line for the <see cref="BuildInfoViewModel"/>.
+		/// Gets the status for the <see cref="BuildInfoViewModel"/>.
 		/// </summary>
-	    private string bottomLine;
+		public BuildStatus Status { get; private set; }
 
-        /// <summary>
-        /// The status for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        private BuildStatus status;
+		/// <summary>
+		/// Gets the description for the <see cref="BuildInfoViewModel"/>.
+		/// </summary>
+		public string Description { get; private set; }
 
-        /// <summary>
-        /// A static synchronisation lock used to synchronise access across all instances.
-        /// </summary>
-        private static object synchronisationLock = new object();
+		/// <summary>
+		/// Gets the name of the person who requested the <see cref="BuildInfoViewModel"/>.
+		/// </summary>
+		public string RequestedBy { get; private set; }
 
-	    #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Gets the status for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        public BuildStatus Status
-        {
-            get
-            {
-                return this.status;
-            }
-        }
-
-        /// <summary>
-        /// Gets the description for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        public string Description
-        {
-            get
-            {
-                return this.description;
-            }
-        }
-
-        /// <summary>
-        /// Gets the name of the person who requested the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        public string RequestedBy
-        {
-            get
-            {
-                return this.requestedBy;
-            }
-        }
-
-        /// <summary>
-        /// Gets the time when the build was started.
-        /// </summary>
-        public DateTime? StartedOn
-        {
-            get
-            {
-                return this.startedOn;
-            }
-        }
+		/// <summary>
+		/// Gets the time when the build was started.
+		/// </summary>
+		public DateTime? StartedOn { get; private set; }
 
 		/// <summary>
 		/// Gets the formatted string for status.
 		/// </summary>
-		public string LatestStatus
-		{
-			get
-			{
-				return this.latestStatus;
-			}
-		}
+		public string LatestStatus { get; private set; }
 
-        /// <summary>
-        /// Gets the time the build was completed.
-        /// </summary>
-        public DateTime? CompletedOn
-        {
-            get
-            {
-                return this.completedOn;
-            }
-        }
+		/// <summary>
+		/// Gets the time the build was completed.
+		/// </summary>
+		public DateTime? CompletedOn { get; private set; }
 
-        /// <summary>
-        /// Gets the test results for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        public string TestResults
-        {
-            get
-            {
-                return this.testResults;
-            }
-        }
+		/// <summary>
+		/// Gets the test results for the <see cref="BuildInfoViewModel"/>.
+		/// </summary>
+		public string TestResults { get; private set; }
 
 		/// <summary>
 		/// Gets the bottom line for the <see cref="BuildInfoViewModel"/>.
 		/// </summary>
-		public string BottomLine
+		public string BottomLine { get; private set; }
+
+		/// <summary>
+		/// Gets the test results for the <see cref="BuildInfoViewModel"/>.
+		/// </summary>
+		public string FailedTests { get; private set; }
+
+		/// <summary>
+		/// Gets a value that indicates whether the build has any builds queued.
+		/// </summary>
+		public bool IsQueued
 		{
 			get
 			{
-				return this.bottomLine;
+				return this.DataModel.IsQueued;
 			}
 		}
 
-        /// <summary>
-        /// Gets a value that indicates whether the build has any builds queued.
-        /// </summary>
-        public bool IsQueued
-        {
-            get
-            {
-                return this.dataModel.IsQueued;
-            }
-        }
+		/// <summary>
+		/// Gets a value indicating whether the current build was completed more than a week ago.
+		/// </summary>
+		public bool IsStale
+		{
+			get
+			{
+				return this.DataModel.IsStale;
+			}
+		}
 
-        /// <summary>
-        /// Gets a value indicating whether the current build was completed more than a week ago.
-        /// </summary>
-        public bool IsStale
-        {
-            get
-            {
-                return this.dataModel.IsStale;
-            }
-        }
+		/// <summary>
+		/// Gets or sets a value indicating whether the build completed with warnings.
+		/// </summary>
+		public bool HasWarnings
+		{
+			get; set;
+		}
 
-        /// <summary>
-        /// Gets or sets a value indicating whether the build completed with warnings.
-        /// </summary>
-        public bool HasWarnings
-        {
-            get; set;
-        }
+		#endregion
 
-        #endregion
+		#region Constructors
 
-        #region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BuildInfoViewModel"/> class.
+		/// </summary>
+		/// <param name="buildInfoModel">The model underlying the <see cref="BuildInfoViewModel"/>.</param>
+		public BuildInfoViewModel(BuildInfoModel buildInfoModel)
+		{
+			this.Init(buildInfoModel);
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BuildInfoViewModel"/> class. Uses
-        /// <see cref="Microsoft.TeamFoundation.Build.Client.InformationNodeConverters.GetConfigurationSummary"/>
-        /// to resolve the configuration summary for the <see cref="BuildInfoViewModel"/>.
-        /// </summary>
-        /// <param name="dataModel">The model underlying the <see cref="BuildInfoViewModel"/>.</param>
-        public BuildInfoViewModel(BuildInfoModel dataModel)
-        {
-            this.Init(dataModel);
-        }
+		#endregion
 
-        #endregion
+		#region Methods
 
-        #region Methods
+		/// <summary>
+		/// Initializes the <see cref="BuildInfoViewModel"/> with the specified <see cref="BuildInfoModel"/>.
+		/// </summary>
+		/// <param name="buildInfoModel">The model that is encapsulated by the <see cref="BuildInfoViewModel"/>.</param>
+		private void Init(BuildInfoModel buildInfoModel)
+		{
+			this.DataModel = buildInfoModel;
+			this.DataModel.PropertyChanged += (sender, e) => this.UpdateFromModel();
 
-        /// <summary>
-        /// Initializes the <see cref="BuildInfoViewModel"/> with the specified <see cref="BuildDetailDataModel"/>.
-        /// </summary>
-        /// <param name="dataModel">The model that is encapsulated by the <see cref="BuildInfoViewModel"/>.</param>
-        private void Init(BuildInfoModel dataModel)
-        {
-            this.dataModel = dataModel;
-            this.dataModel.PropertyChanged += delegate(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-            {
-                this.UpdateFromModel();
-            };
+			this.Description = this.DataModel.Description;
+			this.Status = BuildStatus.Loading;
+			this.LatestStatus = BuildStatus.Loading.ToFriendlyString();
 
-            this.description = this.dataModel.Description;
-            this.status = BuildStatus.Loading;
-			this.latestStatus = BuildStatus.Loading.ToFriendlyString();
+			this.HasWarnings = false;
+		}
 
-            this.HasWarnings = false;
-        }
+		/// <summary>
+		/// Updates the view model from the model.
+		/// </summary>
+		private void UpdateFromModel()
+		{
+			bool hasWarnings = false;
 
-        /// <summary>
-        /// Updates the view model from the model.
-        /// </summary>
-        private void UpdateFromModel()
-        {
-            bool hasWarnings = false;
+			this.Description = this.DataModel.Description;
+			this.Status = BuildStatus.NoneFound;
+			this.RequestedBy = null;
+			this.StartedOn = null;
+			this.CompletedOn = null;
+			this.TestResults = null;
+			this.FailedTests = null;
+			this.LatestStatus = BuildStatus.NoneFound.ToFriendlyString();
 
-	        this.description = this.dataModel.Description;
-            this.status = BuildStatus.NoneFound;
-            this.requestedBy = null;
-            this.startedOn = null;
-            this.completedOn = null;
-            this.testResults = null;
-			this.latestStatus = BuildStatus.NoneFound.ToFriendlyString();
+			if (this.DataModel.Model != null)
+			{
+				this.Status = this.DataModel.Model.Status;
 
-            if (this.dataModel.Model != null)
-            {
-                this.status = this.dataModel.Model.Status;
+				this.RequestedBy = this.DataModel.Model.RequestedFor;
 
-                this.requestedBy = this.dataModel.Model.RequestedFor;
-
-                // remove domain prefix from requestedBy, if found
-                if (!string.IsNullOrEmpty(this.requestedBy))
-                {
-                    this.requestedBy = this.requestedBy.Contains("\\")
-                                           ? this.requestedBy.Split('\\')[1]
-                                           : this.requestedBy;
-                }
-				this.startedOn = this.dataModel.Model.StartTime;
-	            if (dataModel.Model.Status == BuildStatus.InProgress)
-	            {
+				// remove domain prefix from requestedBy, if found
+				if (!string.IsNullOrEmpty(this.RequestedBy))
+				{
+					this.RequestedBy = this.RequestedBy.Contains("\\")
+										   ? this.RequestedBy.Split('\\')[1]
+										   : this.RequestedBy;
+				}
+				this.StartedOn = this.DataModel.Model.StartTime;
+				if (DataModel.Model.Status == BuildStatus.InProgress)
+				{
 					UpdateInProgress();
-	            }
-	            else
-	            {
-		            this.latestStatus = this.dataModel.Model.Status.ToFriendlyString();
-	            }
+				}
+				else
+				{
+					this.LatestStatus = this.DataModel.Model.Status.ToFriendlyString();
+				}
 
-	            if (this.dataModel.Model.BuildFinished)
-	            {
-		            UpdateFinished();
+				if (this.DataModel.Model.BuildFinished)
+				{
+					UpdateFinished();
 
-		            hasWarnings = this.dataModel.Model.HasWarnings;
-	            }
-            }
+					hasWarnings = this.DataModel.Model.HasWarnings;
+				}
+			}
 
-            this.HasWarnings = hasWarnings;
+			this.HasWarnings = hasWarnings;
 
-            this.OnPropertyChanged("IsQueued");
-            this.OnPropertyChanged("IsStale");
-            this.OnPropertyChanged("Status");
-            this.OnPropertyChanged("Description");
-            this.OnPropertyChanged("RequestedBy");
-            this.OnPropertyChanged("StartedOn");
-            this.OnPropertyChanged("CompletedOn");
-            this.OnPropertyChanged("TestResults");
+			this.OnPropertyChanged("IsQueued");
+			this.OnPropertyChanged("IsStale");
+			this.OnPropertyChanged("Status");
+			this.OnPropertyChanged("Description");
+			this.OnPropertyChanged("RequestedBy");
+			this.OnPropertyChanged("StartedOn");
+			this.OnPropertyChanged("CompletedOn");
+			this.OnPropertyChanged("TestResults");
+			this.OnPropertyChanged("FailedTests");
 			this.OnPropertyChanged("HasWarnings");
 			this.OnPropertyChanged("LatestStatus");
 			this.OnPropertyChanged("BottomLine");
 		}
 
-	    private void UpdateFinished()
-	    {
-		    TimeSpan buildTime = (TimeSpan) (this.dataModel.Model.FinishTime - this.dataModel.Model.StartTime);
-		    this.latestStatus = String.Format("{0} {1:d} {1:t} ({2} min)", this.dataModel.Model.Status.ToFriendlyString(),
-			    this.dataModel.Model.FinishTime, Math.Round(buildTime.TotalMinutes));
-		    this.completedOn = this.dataModel.Model.FinishTime;
+		private void UpdateFinished()
+		{
+			TimeSpan buildTime = (TimeSpan) (this.DataModel.Model.FinishTime - this.DataModel.Model.StartTime);
+			this.LatestStatus = String.Format("{0} {1:d} {1:t} ({2} min)", this.DataModel.Model.Status.ToFriendlyString(),
+				this.DataModel.Model.FinishTime, Math.Round(buildTime.TotalMinutes));
+			this.CompletedOn = this.DataModel.Model.FinishTime;
 
-		    if (this.dataModel.Model.TestsTotal.HasValue && this.dataModel.Model.TestsTotal > 0)
-		    {
-			    this.testResults = string.Format("Test results: {0} passed, {1} failed, {2} total.",
-				    this.dataModel.Model.TestsPassed, this.dataModel.Model.TestsFailed,
-				    this.dataModel.Model.TestsTotal);
+			if (this.DataModel.Model.TestsTotal.HasValue && this.DataModel.Model.TestsTotal > 0)
+			{
+				this.TestResults = string.Format("Test results: {0} passed, {1} failed, {2} total.",
+					this.DataModel.Model.TestsPassed, this.DataModel.Model.TestsFailed,
+					this.DataModel.Model.TestsTotal);
+				if (DataModel.Model.FailedTests.Count > 0)
+				{
+					this.BottomLine = "Failed tests: ";
+					foreach (string failedTestName in this.DataModel.Model.FailedTests)
+					{
+						this.BottomLine += failedTestName + " ";
+					}
+				}
+				else
+				{
+					this.BottomLine = string.Empty;
+				}
+				if (this.DataModel.Model.TestsFailed > 0)
+				{
+					this.FailedTests = this.DataModel.Model.TestsFailed.ToString();
+				}
+			}
+			else
+			{
+				this.TestResults = string.Empty;
+				if (this.Status == BuildStatus.PartiallySucceeded)
+				{
+					this.FailedTests = "?";
+				}
+			}
 
-			    if (dataModel.Model.FailedTests.Count > 0)
-			    {
-				    this.bottomLine = "Failed tests: ";
-				    foreach (string failedTestName in this.dataModel.Model.FailedTests)
-				    {
-					    this.bottomLine += failedTestName + " ";
-				    }
-			    }
-			    else
-			    {
-				    this.bottomLine = string.Empty;
-			    }
-		    }
-		    else
-		    {
-			    this.testResults = string.Empty;
-		    }
+			if (this.DataModel.Model.CodeCoverage.HasValue)
+			{
+				this.TestResults += string.Format(" Code Coverage: {0}%", this.DataModel.Model.CodeCoverage);
+			}
+		}
 
-		    if (this.dataModel.Model.CodeCoverage.HasValue)
-		    {
-			    this.testResults += string.Format(" Code Coverage: {0}%", this.dataModel.Model.CodeCoverage);
-		    }
-	    }
+		private void UpdateInProgress()
+		{
+			TimeSpan buildTime = (TimeSpan) (DateTime.Now - this.DataModel.Model.StartTime);
+			this.LatestStatus = String.Format("{0} {1:d} {1:t} ({2} min)", this.DataModel.Model.Status.ToFriendlyString(),
+				this.DataModel.Model.StartTime, Math.Round(buildTime.TotalMinutes));
+			switch (DataModel.Model.CompilationStatus)
+			{
+				case BuildPhaseStatus.Failed:
+					this.TestResults = String.Format("Compilation Failed");
+					break;
+				case BuildPhaseStatus.Succeeded:
+					this.TestResults = String.Format("Compilation Succeeded");
+					break;
+				case BuildPhaseStatus.Unknown:
+					break;
+			}
+			switch (DataModel.Model.TestStatus)
+			{
+				case BuildPhaseStatus.Failed:
+					this.TestResults += String.Format(" Test Failed");
+					break;
+				case BuildPhaseStatus.Succeeded:
+					this.TestResults += String.Format(" Test Succeeded");
+					break;
+				case BuildPhaseStatus.Unknown:
+					break;
+			}
+			this.BottomLine = string.Empty;
+		}
 
-	    private void UpdateInProgress()
-	    {
-		    TimeSpan buildTime = (TimeSpan) (DateTime.Now - this.dataModel.Model.StartTime);
-		    this.latestStatus = String.Format("{0} {1:d} {1:t} ({2} min)", this.dataModel.Model.Status.ToFriendlyString(),
-			    this.dataModel.Model.StartTime, Math.Round(buildTime.TotalMinutes));
-		    switch (dataModel.Model.CompilationStatus)
-		    {
-			    case BuildPhaseStatus.Failed:
-				    this.testResults = String.Format("Compilation Failed");
-				    break;
-			    case BuildPhaseStatus.Succeeded:
-				    this.testResults = String.Format("Compilation Succeeded");
-				    break;
-			    case BuildPhaseStatus.Unknown:
-				    break;
-		    }
-		    switch (dataModel.Model.TestStatus)
-		    {
-			    case BuildPhaseStatus.Failed:
-				    this.testResults += String.Format(" Test Failed");
-				    break;
-			    case BuildPhaseStatus.Succeeded:
-				    this.testResults += String.Format(" Test Succeeded");
-				    break;
-			    case BuildPhaseStatus.Unknown:
-				    break;
-		    }
-			this.bottomLine = string.Empty;
-	    }
+		/// <summary>
+		/// Raises the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
+		/// </summary>
+		/// <param name="propertyName">The name of the property that changed.</param>
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 
-	    /// <summary>
-        /// Raises the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
-        /// </summary>
-        /// <param name="propertyName">The name of the property that changed.</param>
-        private void OnPropertyChanged(string propertyName)
-        {
-            if (this.PropertyChanged != null)
-            {
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+		public static BuildInfoViewModel FromString(string s, IBuildServerService service)
+		{
+			return new BuildInfoViewModel(BuildInfoModel.FromString(s, service));
+		}
 
-        public static BuildInfoViewModel FromString(string s, IBuildServerService service)
-        {
-            return new BuildInfoViewModel(BuildInfoModel.FromString(s, service));
-        }
+		#endregion
 
-        #endregion
+		#region INotifyPropertyChanged Members
 
-        #region INotifyPropertyChanged Members
+		/// <summary>
+		/// Occurs when a property value changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
-    }
+		#endregion
+	}
 }
