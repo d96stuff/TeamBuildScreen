41ed6701028643852f29aef16e6bfa0749d22158
StaffanW_cp <SND\StaffanW_cp>
2015-09-08 23:03:21 +0000

    * Support for TFS 2015! Note: Installer not finished yet.
    This could be the version to rule them all, with the object model replaced by the new ExtendedClient NuGet package from MS. This new binary should be compatible with earlier versions of TFS (not sure about 2008), and not have any prerequisites about installing any object model or VS on the client.

---
+++ Main/Source/TeamBuildScreen.Tfs2015/Models/Tfs2015Manifest.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamBuildScreen.Tfs2015.Models
+{
+	using System.ComponentModel.Composition;
+
+	using TeamBuildScreen.Core.Models;
+
+	[Export(typeof(IServerManifest))]
+	public class Tfs2015Manifest : ServerManifest<BuildServerService, TeamProjectPickerAdapter>
+	{
+		public Tfs2015Manifest() : base("Team Foundation Server 2015") { }
+	}
+}
